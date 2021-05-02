c76e0c7d819c8f8ba1622f5c6fc4ae530535f860
Vp <vp2b2t@gmail.com>
2021-05-02 12:08:46 +0300

    DiscordRPC fix

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/BleachHack.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/BleachHack.java
@@ -38,7 +38,7 @@ public class BleachHack implements ModInitializer {
 	public static Logger logger;
 
 	public static final String VERSION = "1.2.1";
-	public static final int INTVERSION = 29;
+	public static final int INTVERSION = 28;
 
 	public static final EventBus eventBus = new EventBus();
 
--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/Suicide.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/Suicide.java
@@ -2,12 +2,11 @@ package bleach.hack.module.mods;
 
 import bleach.hack.module.Category;
 import bleach.hack.module.Module;
-import bleach.hack.setting.base.SettingSlider;
 
 public class Suicide extends Module {
 
     public Suicide() {
-        super ("Suicide", KEY_UNBOUND, Category.PLAYER, "Automatically types /kill in chat (ONLY WORKS ON ANARCHY SERVER)");
+        super ("Suicide", KEY_UNBOUND, Category.PLAYER, "Automatically types /kill in chat (ONLY WORKS ON ANARCHY SERVERS)");
 
     }
 
