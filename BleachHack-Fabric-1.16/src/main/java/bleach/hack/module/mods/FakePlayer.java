39b1d1401960c2b105db5ca625e939cdc2dc5807
Vp <vp2b2t@gmail.com>
2021-05-06 23:27:20 +0300

    Added FakePlayer

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/FakePlayer.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/FakePlayer.java
@@ -1,46 +1,45 @@
-///TODO FakePlayer
-/// package bleach.hack.module.mods;
-// import bleach.hack.module.Category;
-// import bleach.hack.module.Module;
-// import bleach.hack.util.world.FakePlayerUtil;
-// import net.minecraft.util.math.Vec3d;
+package bleach.hack.module.mods;
 
-// public class FakePlayer extends Module {
+import bleach.hack.module.Category;
+import bleach.hack.module.Module;
+import net.minecraft.util.math.Vec3d;
 
-//    private FakePlayerUtil fake;
-//    private Vec3d oldPos;
-//    private Vec3d newPos;
+public class FakePlayer extends Module {
 
-//    public FakePlayer() {
-//        super("FakePlayer", KEY_UNBOUND, Category.MISC, "Creates fake player");
-//    }
+    private bleach.hack.util.world.FakePlayerUtil fake;
+    private Vec3d oldPos;
+    private Vec3d newPos;
 
-//    public void
-//    onEnable()
-//    {
-//        if (mc.player != null) {
-//            oldPos = mc.player.getPos();
+    public FakePlayer() {
+        super("FakePlayer", KEY_UNBOUND, Category.RENDER, "Creates fake player");
+    }
+
+    public void
+    onEnable()
+    {
+        if (mc.player != null) {
+            oldPos = mc.player.getPos();
 
             // fake.setAbsorptionAmount(69);
-//            fake = new FakePlayerUtil();
-//            fake.copyPositionAndRotation(mc.player);
-//            fake.copyFrom(mc.player);
-//            fake.setBoundingBox(fake.getBoundingBox().expand(0.1));
-//            fake.spawn();
-//            fake.abilities.invulnerable = true;
-//        }
-//        super.onEnable();
-//    }
+            fake = new bleach.hack.util.world.FakePlayerUtil();
+            fake.copyPositionAndRotation(mc.player);
+            fake.copyFrom(mc.player);
+            fake.setBoundingBox(fake.getBoundingBox().expand(0.1));
+            fake.spawn();
+            fake.abilities.invulnerable = true;
+        }
+        super.onEnable();
+    }
 
-//    public void
-//    onDisable()
-//    {
-//        if (fake != null) {
-//            newPos = mc.player.getPos();
-//            fake.despawn();
-//            mc.player.setPos(newPos.x, newPos.y, newPos.z);
-//        }
-//        super.onDisable();
-//    }
+    public void
+    onDisable()
+    {
+        if (fake != null) {
+            newPos = mc.player.getPos();
+            fake.despawn();
+            mc.player.setPos(newPos.x, newPos.y, newPos.z);
+        }
+        super.onDisable();
+    }
 
-// }
+}
--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/util/world/FakePlayerUtil.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/util/world/FakePlayerUtil.java
@@ -1,35 +1,35 @@
-///TODO FakePlayer
-//package bleach.hack.util.world;
-//
-//import com.mojang.authlib.GameProfile;
-//import net.minecraft.client.MinecraftClient;
-//import net.minecraft.client.network.OtherClientPlayerEntity;
 
-//public class FakePlayerUtil extends OtherClientPlayerEntity
-//    {
-//    public FakePlayerUtil()
-//        {
-//            this(MinecraftClient.getInstance().player.getGameProfile());
-//        }
+package bleach.hack.util.world;
 
-//    public FakePlayerUtil(GameProfile profile)
-//        {
-//            this(profile, MinecraftClient.getInstance().player.getX(), MinecraftClient.getInstance().player.getY(), MinecraftClient.getInstance().player.getZ());
-//        }
-//
-//    public FakePlayerUtil(GameProfile profile, double x, double y, double z)
-//        {
-//            super(MinecraftClient.getInstance().world, profile);
-//            setPos(x, y, z);
-//        }
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.OtherClientPlayerEntity;
 
-//        public void spawn ()
-//        {
-//            MinecraftClient.getInstance().world.addEntity(this.getEntityId(), this);
-//        }
+public class FakePlayerUtil extends OtherClientPlayerEntity
+    {
+    public FakePlayerUtil()
+        {
+            this(MinecraftClient.getInstance().player.getGameProfile());
+        }
 
-//        public void despawn ()
-//        {
-//            MinecraftClient.getInstance().world.removeEntity(this.getEntityId());
-//        }
-//    }
+    public FakePlayerUtil(GameProfile profile)
+        {
+            this(profile, MinecraftClient.getInstance().player.getX(), MinecraftClient.getInstance().player.getY(), MinecraftClient.getInstance().player.getZ());
+        }
+
+    public FakePlayerUtil(GameProfile profile, double x, double y, double z)
+        {
+            super(MinecraftClient.getInstance().world, profile);
+            setPos(x, y, z);
+        }
+
+        public void spawn ()
+        {
+            MinecraftClient.getInstance().world.addEntity(this.getEntityId(), this);
+        }
+
+        public void despawn ()
+        {
+            MinecraftClient.getInstance().world.removeEntity(this.getEntityId());
+        }
+    }
