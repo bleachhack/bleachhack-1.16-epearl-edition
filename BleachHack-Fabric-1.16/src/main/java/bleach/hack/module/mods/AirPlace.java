0ae9bec29d870e707be851e3fdbeebdcc5599dea
Vp <vp2b2t@gmail.com>
2021-05-22 22:08:59 +0300

    Renamed PlaceInAir to AirPlace

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/AirPlace.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/AirPlace.java
@@ -6,7 +6,7 @@ import bleach.hack.event.events.EventSendPacket;
 import bleach.hack.event.events.EventTick;
 import bleach.hack.event.events.EventWorldRender;
 import bleach.hack.mixin.AccessorMinecraftClient;
-import bleach.hack.module.Category;
+import bleach.hack.module.ModuleCategory;
 import bleach.hack.module.Module;
 import bleach.hack.setting.base.SettingColor;
 import bleach.hack.setting.base.SettingMode;
@@ -25,12 +25,12 @@ import net.minecraft.util.math.BlockPos;
  * @author <a href="https://github.com/lasnikprogram">Lasnik</a>
  */
 
-public class PlaceInAir extends Module {
+public class AirPlace extends Module {
 
     private boolean pressed;
 
-    public PlaceInAir() {
-        super("PlaceInAir", KEY_UNBOUND, Category.WORLD, "Allows you to place blocks in thin air",
+    public AirPlace() {
+        super("AirPlace", KEY_UNBOUND, ModuleCategory.WORLD, "Allows you to place blocks in thin air",
                 new SettingToggle("Highlight", true).withDesc("Renders an overlay where it will place the block").withChildren(
                         new SettingMode("Render", "Box+Fill", "Box", "Fill").withDesc("The rendering method"),
                         new SettingSlider("Box", 0.1, 4, 2, 1).withDesc("The thickness of the box lines"),
