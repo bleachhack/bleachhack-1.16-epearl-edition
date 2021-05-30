f98b3a07300aee244a05120a221e249e49c24080
Vp <vp2b2t@gmail.com>
2021-05-30 13:40:11 +0300

    1.3-DevBuild 1.0: -Added BetterCamera (Made by CUPZYY)

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/mixin/MixinCamera.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/mixin/MixinCamera.java
@@ -1,6 +1,9 @@
 package bleach.hack.mixin;
 
+import bleach.hack.module.Module;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -14,10 +17,35 @@ import net.minecraft.fluid.Fluids;
 @Mixin(Camera.class)
 public class MixinCamera {
 
+	@Unique
+	private boolean bypassCameraClip;
+
+	@Shadow
+	private double clipToSpace(double desiredCameraDistance) { return 0; }
+
 	@Inject(method = "getSubmergedFluidState", at = @At("HEAD"), cancellable = true)
 	private void getSubmergedFluidState(CallbackInfoReturnable<FluidState> cir) {
 		if (((NoRender) ModuleManager.getModule("NoRender")).shouldRemoveOverlay(3)) {
 			cir.setReturnValue(Fluids.EMPTY.getDefaultState());
+		}
+	}
+
+	@Inject(method = "clipToSpace", at = @At("HEAD"), cancellable = true)
+	private void onClipToSpace(double desiredCameraDistance, CallbackInfoReturnable<Double> info) {
+		if (bypassCameraClip) {
+			bypassCameraClip = false;
+		} else {
+			Module betterCamera = ModuleManager.getModule("BetterCamera");
+
+			if (betterCamera.isEnabled()) {
+				if (betterCamera.getSetting(0).asToggle().state) {
+					info.setReturnValue(betterCamera.getSetting(1).asToggle().state
+							? betterCamera.getSetting(1).asToggle().getChild(0).asSlider().getValue() : desiredCameraDistance);
+				} else if (betterCamera.getSetting(1).asToggle().state) {
+					bypassCameraClip = true;
+					clipToSpace(desiredCameraDistance);
+				}
+			}
 		}
 	}
 }
--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/XCarry.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/XCarry.java
@@ -32,10 +32,12 @@ public class XCarry extends Module {
 
     @Subscribe
     private void onEventSend(EventSendPacket event){
-        if (event.getPacket() instanceof CloseHandledScreenC2SPacket) {
+        if (event.getPacket() instanceof CloseHandledScreenC2SPacket)
+            return;
+        {
             if (((CloseHandledScreenC2SPacketAccessor) event.getPacket()).getSyncId() == mc.player.playerScreenHandler.syncId) {
                 invOpened = true;
-                event.isCancelled();
+                event.setCancelled(true);
             }
         }
     }
