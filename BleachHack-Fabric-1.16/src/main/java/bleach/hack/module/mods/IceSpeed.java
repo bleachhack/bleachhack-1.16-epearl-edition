64065d5fcc029b0f7d3c0c61972f9f9120fb6797
Vp <vp2b2t@gmail.com>
2021-06-13 23:47:07 +0300

    Icespeed but 10times better

--- BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/IceSpeed.java
+++ BleachHack-Fabric-1.16/src/main/java/bleach/hack/module/mods/IceSpeed.java
@@ -4,44 +4,39 @@ import bleach.hack.event.events.EventTick;
 import bleach.hack.module.Module;
 import bleach.hack.module.ModuleCategory;
 import bleach.hack.setting.base.SettingSlider;
-import bleach.hack.util.ReflectionHelper;
+import bleach.hack.util.FabricReflect;
+import com.google.common.collect.Sets;
 import com.google.common.eventbus.Subscribe;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 
-import java.util.Arrays;
-
-/**
- * @Author Vp
- * https://github.com/HerraVp
- */
-
-//This is a modified version of Ares client IceSpeed
+import java.util.Set;
 
 public class IceSpeed extends Module {
-//Its really scuffed but it works
+
+    private static final Set<Block> ICE_BLOCKS = Sets.newHashSet(Blocks.ICE, Blocks.PACKED_ICE, Blocks.FROSTED_ICE, Blocks.BLUE_ICE);
 
     public IceSpeed() {
         super("IceSpeed", KEY_UNBOUND, ModuleCategory.MOVEMENT, "Move faster on ice",
-            new SettingSlider("Speed", 0.5f, 0.2f, 0, 1 ).withDesc("Speed value (Lower = faster)"));
+                new SettingSlider("Speed", 0, 9, 4, 2).withDesc("How fast to move when on ice"));
     }
 
     @Subscribe
     public void onTick(EventTick event) {
-        setSlipperiness((float)getSetting(0).asSlider().getValue());
+        setSlipperiness(1f - getSetting(0).asSlider().getValueFloat() / 10f);
     }
 
     @Override
     public void onDisable() {
-        setSlipperiness(0.98f);
-        ReflectionHelper.setPrivateValue(AbstractBlock.class, Blocks.BLUE_ICE, 0.989f, "slipperiness", "field_23163");
+        setSlipperiness(0.989f);
         super.onDisable();
     }
 
-    private void setSlipperiness(float speed) {
-        for(Block block: Arrays.asList(Blocks.ICE, Blocks.PACKED_ICE, Blocks.FROSTED_ICE, Blocks.BLUE_ICE)){
-            ReflectionHelper.setPrivateValue(AbstractBlock.class, block, speed, "slipperiness", "field_23163");
+    private void setSlipperiness(float slipperiness) {
+        for (Block block: ICE_BLOCKS) {
+            if (block.getSlipperiness() != slipperiness) {
+                FabricReflect.writeField(block, slipperiness, "slipperiness", "field_23163");
+            }
         }
     }
 }
