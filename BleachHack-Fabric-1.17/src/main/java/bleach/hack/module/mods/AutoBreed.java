f867ba5c81a6fc9b2b813b812c9c539a5b290b36
Vp <vp2b2t@gmail.com>
2021-06-30 15:56:13 +0300

    Added feedBabies mode to AutoBreed

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/AutoBreed.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/AutoBreed.java
@@ -5,6 +5,7 @@ import bleach.hack.eventbus.BleachSubscribe;
 import bleach.hack.module.Module;
 import bleach.hack.module.ModuleCategory;
 import bleach.hack.setting.base.SettingSlider;
+import bleach.hack.setting.base.SettingToggle;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.util.Hand;
@@ -14,7 +15,8 @@ public class AutoBreed extends Module {
 
     public AutoBreed() {
         super("AutoBreed", KEY_UNBOUND, ModuleCategory.WORLD, "Automatically breeds nearby animals",
-                new SettingSlider("Range", 0, 10, 5, 2));
+                new SettingSlider("Range", 0, 10, 5, 2),
+                new SettingToggle("FeedBabies", false).withDesc("Feeds babies"));
     }
 
     @BleachSubscribe
@@ -24,7 +26,7 @@ public class AutoBreed extends Module {
             if (e != null && e instanceof AnimalEntity) {
                 final AnimalEntity animal = (AnimalEntity) e;
                 if (animal.getHealth() > 0) {
-                    if (!animal.isBaby() && !animal.isInLove() && mc.player.distanceTo(animal) <= getSetting(0).asSlider().getValue() && animal.isBreedingItem(mc.player.getInventory().getMainHandStack())) {
+                    if (!animal.isBaby() || getSetting(1).asToggle().state && !animal.isInLove() && mc.player.distanceTo(animal) <= getSetting(0).asSlider().getValue() && animal.isBreedingItem(mc.player.getInventory().getMainHandStack())) {
                         mc.interactionManager.interactEntity(mc.player, animal, Hand.MAIN_HAND);
                     }
                 }
