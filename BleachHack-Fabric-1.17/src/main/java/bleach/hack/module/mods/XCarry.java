b9c438037d554570af076b7110a66f20c608a0b2
Vp <vp2b2t@gmail.com>
2021-07-04 21:12:10 +0300

    Finally got XCarry to work LOL BRUH BRUAH

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/XCarry.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/XCarry.java
@@ -5,7 +5,6 @@ import bleach.hack.eventbus.BleachSubscribe;
 import bleach.hack.mixin.CloseHandledScreenC2SPacketAccessor;
 import bleach.hack.module.ModuleCategory;
 import bleach.hack.module.Module;
-import com.google.common.eventbus.Subscribe;
 import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
 
 /**
@@ -19,24 +18,10 @@ public class XCarry extends Module {
         super("XCarry", KEY_UNBOUND, ModuleCategory.PLAYER, "Allows you to store extra items in your crafting slots");
     }
 
-    @Override
-    public void onEnable() {
-        invOpened = false;
-    }
-
-    @Override
-    public void onDisable() {
-        if (invOpened) {
-            assert mc.player != null;
-            mc.player.networkHandler.sendPacket(new CloseHandledScreenC2SPacket(mc.player.playerScreenHandler.syncId));
-        }
-    }
-
     @BleachSubscribe
-    private void onEventSend(EventSendPacket event){
+    public void onSendPacket(EventSendPacket event){
         if (!(event.getPacket() instanceof CloseHandledScreenC2SPacket))
             return;
-        assert mc.player != null;
         if (((CloseHandledScreenC2SPacketAccessor) event.getPacket()).getSyncId() == mc.player.playerScreenHandler.syncId) {
             invOpened = true;
             event.setCancelled(true);
