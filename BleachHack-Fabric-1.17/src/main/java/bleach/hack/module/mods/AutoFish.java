36a3ef1acc27698e8e3771e9b364d75b400e1695
Vp <vp2b2t@gmail.com>
2021-07-07 12:55:29 +0300

    Added option to choose which hand to use in AutoFish

--- BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/AutoFish.java
+++ BleachHack-Fabric-1.17/src/main/java/bleach/hack/module/mods/AutoFish.java
@@ -5,8 +5,8 @@ import bleach.hack.event.events.EventTick;
 import bleach.hack.eventbus.BleachSubscribe;
 import bleach.hack.module.ModuleCategory;
 import bleach.hack.module.Module;
+import bleach.hack.setting.base.SettingMode;
 import bleach.hack.setting.base.SettingSlider;
-import com.google.common.eventbus.Subscribe;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
 import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
@@ -14,13 +14,13 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 
 public class AutoFish extends Module {
-
     public static int ticksPassed;
     public static boolean pullFish;
 
     public AutoFish() {
         super("AutoFish", KEY_UNBOUND, ModuleCategory.PLAYER, "Fishes for u",
-                new SettingSlider("Recast Delay", 1, 10, 3, 0));
+                new SettingSlider("RecastDelay", 1, 10, 3, 0),
+                new SettingMode("Hand", "Main", "Off").withDesc("Which hand to use"));
     }
 
     @BleachSubscribe
@@ -41,11 +41,21 @@ public class AutoFish extends Module {
             ticksPassed--;
         }
         if (pullFish && ticksPassed == (int) getSetting(0).asSlider().getValue() * 20 - 5) {
-            mc.player.networkHandler.sendPacket(new PlayerInteractItemC2SPacket(Hand.MAIN_HAND));
+            if (getSetting(1).asMode().mode == 0); {
+                mc.player.networkHandler.sendPacket(new PlayerInteractItemC2SPacket(Hand.MAIN_HAND));
+            }
+            if (getSetting(1).asMode().mode == 1); {
+                mc.player.networkHandler.sendPacket(new PlayerInteractItemC2SPacket(Hand.OFF_HAND));
+            }
             pullFish = false;
         }
         if (ticksPassed == 1) {
-            mc.player.networkHandler.sendPacket(new PlayerInteractItemC2SPacket(Hand.MAIN_HAND));
+            if (getSetting(1).asMode().mode == 0); {
+                mc.player.networkHandler.sendPacket(new PlayerInteractItemC2SPacket(Hand.MAIN_HAND));
+            }
+            if (getSetting(1).asMode().mode == 1); {
+                mc.player.networkHandler.sendPacket(new PlayerInteractItemC2SPacket(Hand.OFF_HAND));
+            }
             ticksPassed = 0;
         }
     }
